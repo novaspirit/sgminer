09bc88214e0cd6da44433988c981224b97727913
djm34 <djm34@aliceadsl.fr>
2015-08-07 11:37:36 +0200

    assign correct size to the buffer

--- algorithm/Lyra2RE.h
+++ algorithm/Lyra2RE.h
@@ -2,8 +2,8 @@
 #define LYRA2RE_H
 
 #include "miner.h"
-#define LYRA_SCRATCHBUF_SIZE (4 ) // matrix extended to 16 matrix[16][8][8] uint64_t or equivalent
-#define LYRA_SECBUF_SIZE (4) //8 uint64
+#define LYRA_SCRATCHBUF_SIZE (1536) // matrix size [12][4][4] uint64_t or equivalent
+#define LYRA_SECBUF_SIZE (4) // (not used)
 extern int lyra2re_test(unsigned char *pdata, const unsigned char *ptarget,
 			uint32_t nonce);
 extern void lyra2re_regenhash(struct work *work);
